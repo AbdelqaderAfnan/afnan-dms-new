bb3b490d2407e43c29200e88cbb3fa46bcde6964
AbdelqaderAfnan <abdelqader@afnangroup.com>
2020-12-21 09:30:42 +0200

    Before JS

--- resources/views/Document/branch/erbil.blade.php
+++ resources/views/Document/branch/erbil.blade.php
@@ -1,2 +1,29 @@
-<?php
-dd('erbil');
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <button class="btn btn-primary text-center" onclick="myFunction()">
+                    Add New Folder
+                </button>
+            </div>
+        </div>        
+        <p id="demo"></p>
+    </div>
+
+    <script>
+        function myFunction()
+        {
+            
+            var folder = prompt("Please enter your name:", "folder name");
+            if (folder == null || folder == "")
+            {
+                txt = "User cancelled the prompt.";
+            }
+            
+            document.getElementById("demo").innerHTML = folder;
+            
+        }
+    </script>
+@endsection
