8810366d89b8131fe82007001ef392d869ce4c36
AbdelqaderAfnan <abdelqader@afnangroup.com>
2020-12-21 12:11:07 +0200

    add js file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,6 +81,9 @@
 
         <main class="py-4">
             @yield('content')
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+            <script src="{{ asset('js/newfolder.js') }}" defer></script>
         </main>
     </div>
 </body>
