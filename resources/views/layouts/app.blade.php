6f387801a424d2e1c236942f4763bf16207b133c
AbdelqaderAfnan <abdelqader@afnangroup.com>
2021-01-09 11:08:57 +0200

    set forget password on log

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,17 +41,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                           
                             
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                            
                         @else
                             <li  class="nav-item">
                                 <a class="nav-link " href="{{url('/')}}">Home</a>
